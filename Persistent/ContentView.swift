f56b3b4ea39b464b96327f396d8715a43cc6fc65
realSkilluminati <54849420+realSkilluminati@users.noreply.github.com>
2021-06-25 15:00:16 +0200

    Initial Commit

---
+++ Persistent/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Persistent
+//
+//  Created by Bennett Quaritsch on 25.06.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Persistent/PersistentApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersistentApp.swift
+//  Persistent
+//
+//  Created by Bennett Quaritsch on 25.06.21.
+//
+
+import SwiftUI
+
+@main
+struct PersistentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
